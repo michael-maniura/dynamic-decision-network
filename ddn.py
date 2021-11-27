e9c6769db1397fbb75140f5bb8a8f1a6ff488a33
maniuram <michael.maniura@dmgmori.com>
2021-11-27 16:43:59 +0100

    expanded implementation added function comments

--- ddn.py
+++ ddn.py
@@ -1,8 +1,14 @@
 from grid_pomdp import GridPOMDP
 import random
+import time
 
 class DynamicDecisionNetwork:
     def __init__(self, max_depth):
+        """
+        Initializer function of the class with
+        a maximum depth parameter for the
+        belief state forward-chaining
+        """
         self.grid_pomdb = self.initialize_grid()
         self.belief_state = {
             (0,0) : 1/9,
@@ -19,9 +25,13 @@ class DynamicDecisionNetwork:
         }
 
         self.max_depth = max_depth
-        self.possible_evidences = [0, 1, 2, 3]
+        self.possible_evidence_indices = [0, 1, 2, 3]
 
     def initialize_grid(self):
+        """
+        Initialize the POMDB grid
+        to solve
+        """
         r = -0.04
         grid = GridPOMDP(
             [[r, r, r, +1],
@@ -31,9 +41,47 @@ class DynamicDecisionNetwork:
         return grid
 
     def solve_grid(self):
-        self.get_maximum_utility_of_belief_state(self.belief_state, 0)
+        """
+        Solve the initialized grid
+        """
+        print("Starting grid solving...")
+        while not self.reached_terminal_state(self.belief_state):
+            best_action, new_belief_state, reward = self.get_maximum_utility_of_belief_state(self.belief_state, 0)
+            self.perform_action_and_update_belief_state(best_action, new_belief_state)
+        print("Solved the grid successfully!")
 
+    def perform_action_and_update_belief_state(self, action, new_belief_state):
+        """
+        Print the best action of a step
+        and the new, assigned belief state.
+        """
+        print("Performing best action: %s" % (action,))
+        print("New belief state:\n%s" % (new_belief_state,))
+        time.sleep(5)
+        self.belief_state = new_belief_state
+
+    def get_maximum_utility_of_belief_state(self, belief_state, depth):
+        """
+        Calculate the maximum utility
+        of a given belief state.
+        This function calls itself recursively
+        with an incrementing depth parameter
+        until the class instance specific
+        maximum depth is reached  
+        """
+        if self.reached_terminal_state(belief_state):
+            return (None, None, self.get_belief_state_reward(belief_state))
+        elif depth == self.max_depth:
+            return (None, None, self.get_belief_state_reward(belief_state))
+        else:
+            best_action, best_new_belief_state = self.get_best_action_and_new_belief_state_for_belief_state(belief_state)
+            return (best_action, best_new_belief_state, self.get_maximum_utility_of_belief_state(best_new_belief_state, depth+1)[2])
+    
     def reached_terminal_state(self, belief_state):
+        """
+        Check if a terminal belief state
+        has been reached.
+        """
         probability_sum = 0
         for terminal in self.grid_pomdb.terminals:
             probability_sum = probability_sum + belief_state[terminal]
@@ -43,57 +91,75 @@ class DynamicDecisionNetwork:
         else:
             return False
 
-    def get_maximum_utility_of_belief_state(self, belief_state, depth):
-        if self.reached_terminal_state(belief_state):
-            return self.get_belief_state_reward(belief_state)
-        elif depth == self.max_depth:
-            return self.get_belief_state_reward(belief_state)
-        else:
-            best_action, best_new_belief_state = self.get_best_action_and_new_belief_state_for_belief_state(belief_state)
-            print("Best action: " + best_action)
-            return self.get_maximum_utility_of_belief_state(best_new_belief_state, depth+1)
-    
-    def get_best_action_and_new_belief_state_for_belief_state(self, current_belief_state):
+    def get_best_action_and_new_belief_state_for_belief_state(self, belief_state):
+        """
+        Get the best action to execute
+        and the resulting belief state
+        from a given belief state
+        """
         best_new_belief_state = None
         best_new_belief_state_reward = 0
         best_action = None
 
+        action_belief_state_reward = []
+
         for action in random.sample(self.grid_pomdb.actlist, len(self.grid_pomdb.actlist)):
-            for evidence in self.possible_evidences:
-                new_possible_belief_state = self.get_new_belief_state(current_belief_state, action, self.grid_pomdb.evidences[evidence])
+            for evidence_index in self.possible_evidence_indices:
+                new_possible_belief_state = self.get_new_belief_state(belief_state, action, evidence_index)
                 belief_state_reward = self.get_belief_state_reward(new_possible_belief_state)
                 
+                action_belief_state_reward.append((action, new_possible_belief_state, belief_state_reward))
+
+                """
                 if belief_state_reward > best_new_belief_state_reward:
                     best_new_belief_state = new_possible_belief_state
                     best_new_belief_state_reward = belief_state_reward
                     best_action = action
+                """
 
+        best_action, best_new_belief_state, best_new_belief_state_reward = max(action_belief_state_reward, key=lambda element:element[2])
         return (best_action, best_new_belief_state)
 
-    def get_new_belief_state(self, current_belief_state, action, evidence):
+    def get_new_belief_state(self, belief_state, action, evidence_index):
+        """
+        Calculate a new belief state
+        from a given belief state
+        for the execution of a given action
+        with given evidence index
+        """
         new_belief_state = {}
         for state in self.grid_pomdb.states:
-            new_state_possibility = self.filter_algorithm(current_belief_state, state, evidence, action)
+            new_state_possibility = self.get_probability_of_new_state_in_new_belief_state(belief_state, state, evidence_index, action)
             new_belief_state[state] = new_state_possibility
+        return new_belief_state
 
-    def filter_algorithm(self, current_belief_state, new_state, evidence, action):
-        normalization_factor = self.get_normalization_factor(current_belief_state, new_state)
-        probability_of_evidence_in_new_state = self.grid_pomdb.evidences[new_state][evidence][0]
+    def get_probability_of_new_state_in_new_belief_state(self, belief_state, new_state, evidence_index, action):
+        """
+        The filter algorithm
+        """
+        normalization_factor = self.get_normalization_factor(belief_state, new_state)
+        probability_of_evidence_in_new_state = self.grid_pomdb.evidences[new_state][evidence_index][0]
 
         state_sum = 0
         for state in self.grid_pomdb.states:
             probability_to_reach_new_state_from_current_state_with_given_action = self.get_probability_to_reach_new_state_from_current_state_with_given_action(new_state, state, action)
-            probability_of_state = current_belief_state[state]
+            probability_of_state = belief_state[state]
             state_sum = state_sum + (probability_to_reach_new_state_from_current_state_with_given_action * probability_of_state)
 
         return normalization_factor * probability_of_evidence_in_new_state * state_sum
 
-    def get_normalization_factor(self, current_belief_state, new_state):
-        current_belief_state_probability_of_new_state = current_belief_state[new_state]
+    def get_normalization_factor(self, belief_state, new_state):
+        """
+        Get the normalization factor
+        for a given belief state and
+        a specific state for the filter
+        algorithm
+        """
+        current_belief_state_probability_of_new_state = belief_state[new_state]
         
         sum_of_possible_follow_up_state_probabilities = 0
         for possible_follow_up_state in self.get_possible_follow_up_states(new_state):
-            sum_of_possible_follow_up_state_probabilities = sum_of_possible_follow_up_state_probabilities + current_belief_state[possible_follow_up_state]
+            sum_of_possible_follow_up_state_probabilities = sum_of_possible_follow_up_state_probabilities + belief_state[possible_follow_up_state]
 
         if sum_of_possible_follow_up_state_probabilities == 0:
             return 0
@@ -101,6 +167,10 @@ class DynamicDecisionNetwork:
             return current_belief_state_probability_of_new_state / sum_of_possible_follow_up_state_probabilities
     
     def get_possible_follow_up_states(self, state):
+        """
+        Get all possible states which can be
+        reached from a given state
+        """
         possible_follow_up_states = []
         
         for action in self.grid_pomdb.transitions[state]:
@@ -116,13 +186,21 @@ class DynamicDecisionNetwork:
         new_state,
         current_state,
         action):
-
+        """
+        Get the probability to reach
+        a new given state from a given
+        current state with a given action
+        """
         for probability, possible_new_state in self.grid_pomdb.transitions[current_state][action]:
             if possible_new_state == new_state:
                 return probability
         return 0
 
     def get_belief_state_reward(self, belief_state):
+        """
+        Get the reward for a
+        given belief state 
+        """
         state_reward_sum = 0
         
         for state in belief_state:
@@ -130,15 +208,18 @@ class DynamicDecisionNetwork:
             weighted_state_reward = state_reward * belief_state[state]
             state_reward_sum = state_reward_sum + weighted_state_reward
         
-        return state_reward_sum 
+        return state_reward_sum
 
-    # bellmann 
+    def get_belief_state_utility(self, belief_state):
+        pass
+
     def get_utility_of_state(self, state, visited_states):
-        # inquire reward of state
+        """
+        The Bellman equation
+        """
         reward_current_state = self.grid_pomdb.rewards[state]
         
         best_follow_up_state_utility = 0
-        # get best follow up state utility (recursion)
         for action in self.grid_pomdb.transitions[state]:
             action_state_transitions = self.grid_pomdb.transitions[state][action]
             for state_transition in action_state_transitions:
@@ -159,5 +240,5 @@ class DynamicDecisionNetwork:
         # return reward of state + (result of above)
         return reward_current_state + best_follow_up_state_utility
 
-ddn = DynamicDecisionNetwork(5)
+ddn = DynamicDecisionNetwork(20)
 ddn.solve_grid()
